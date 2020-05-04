dfd4b2fe4846eb4e8754e3d456d68b5b885dc27b
Lisandra Leal <lisandraleal@iMacdeLisandra.lan>
2020-05-04 03:27:25 +0100

    Created project

---
+++ Proj_2020/cmake-build-debug/CMakeFiles/Proj_2020.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proj_2020.dir/main.cpp.o"
+  "Proj_2020"
+  "Proj_2020.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proj_2020.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Proj_2020/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "   ------   WELCOME TO PORTO CITY TRANSFERS   ------" << endl;
+    cout << endl << "Fast and safe transportation from Campanha train station" << endl;
+
+    //maybe main menu
+    return 0;
+}
